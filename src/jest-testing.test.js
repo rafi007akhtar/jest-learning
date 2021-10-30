67519491760be6765b99101bf8a2a68cd858f9ff
Md Rafi Akhtar <alimdrafi@gmail.com>
2021-10-30 23:29:35 +0530

    done with Setup and Teardown

--- src/jest-testing.test.js
+++ src/jest-testing.test.js
@@ -32,7 +32,7 @@ test('there is no I in team', () => {
     // so, use `toMatch` for testing strings
 });
 test('array inclusivity', () => {
-    const fibonacci = [ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ];  // ... and so on
+    const fibonacci = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];  // ... and so on
     expect(fibonacci).toContain(13);
     expect(new Set(fibonacci)).toContain(1);
     expect(fibonacci).not.toContain(4);
@@ -44,57 +44,73 @@ test('summon the Darkseid', () => {
 });
 // Using Matchers - end
 
-// Testing Asynchronous Code - begin
-// technique 1 - callbacks
-test('the data is peanut butter', (done) => {
-    function callback(data) {
-        try {
-            expect(data).toBe('peanut butter');
-            done();
-        } catch (error) {
-            done(error);
+// Testing Asynchronous Code & Setup and teardown - begin
+// in this section, I am merging both the above mentioned sections, as I think that makes most sense
+
+describe('async and setup / teardown', () => {
+    let data;
+    let errorString;
+    beforeEach(() => {
+        // this will be run before executing each test in this `describe` block
+        data = 'peanut butter';
+        errorString = 'error';
+    });
+    afterAll(() => {
+        // this will run after all the tests have been run in this describe block
+        data = errorString = null;
+    });
+
+    // technique 1 - callbacks
+    test('the data is peanut butter', (done) => {
+        function callback(data) {
+            try {
+                expect(data).toBe(data);
+                done();
+            } catch (error) {
+                done(error);
+            }
         }
-    }
 
-    fetchData(callback);
-});
+        fetchData(callback);
+    });
 
-// technique 2 - for promises
-test('the data from the promise is peanut butter', () => {
-    return fetchDataAsPromise().then(data => {
-        expect(data).toBe('peanut butter');
+    // technique 2 - for promises
+    test('the data from the promise is peanut butter', () => {
+        return fetchDataAsPromise().then(data => {
+            expect(data).toBe(data);
+        })
+    });
+    test('the fetch fails with an error', () => {
+        return fetchErrorAsPromise().catch(e => expect(e).toMatch(errorString));
+    });
+
+    // technique 3 - syntactic sugar for promises
+    test('short - the data from the promise is peanut butter', () => {
+        return expect(fetchDataAsPromise()).resolves.toBe(data);
+    });
+    test('short - the fetch fails with an error', () => {
+        return expect(fetchErrorAsPromise()).rejects.toMatch(errorString);
+    })
+
+    // technique 4 - using async / await
+    // NOTE: unlike Angular, do NOT wrap the tests in an async zone here
+    // instead, just use the `async` keyword to perform the testing
+    test('the data from the promise is peanut butter (async/await)', async () => {
+        const data = await fetchDataAsPromise();
+        expect(data).toBe(data);
+    });
+    test('the fetch fails with an error (async/await)', async () => {
+        try {
+            await fetchErrorAsPromise()
+        } catch (error) {
+            expect(error).toMatch(errorString);
+        }
+    });
+    test('short - the data from the promise is peanut butter (async/await)', async () => {
+        await expect(fetchDataAsPromise()).resolves.toBe(data);
+    });
+    test('short - the fetch fails with an error (async/await)', async () => {
+        await expect(fetchErrorAsPromise()).rejects.toMatch(errorString);
     })
 });
-test('the fetch fails with an error', () => {
-    return fetchErrorAsPromise().catch(e => expect(e).toMatch('error'));
-});
-
-// technique 3 - syntactic sugar for promises
-test('short - the data from the promise is peanut butter', () => {
-    return expect(fetchDataAsPromise()).resolves.toBe('peanut butter');
-});
-test('short - the fetch fails with an error', () => {
-    return expect(fetchErrorAsPromise()).rejects.toMatch('error');
-})
-
-// technique 4 - using async / await
-// NOTE: unlike Angular, do NOT wrap the tests in an async zone here
-// instead, just use the `async` keyword to perform the testing
-test('the data from the promise is peanut butter (async/await)', async () => {
-    const data = await fetchDataAsPromise();
-    expect(data).toBe('peanut butter');
-});
-test('the fetch fails with an error (async/await)', async () => {
-    try {
-        await fetchErrorAsPromise()
-    } catch (error) {
-        expect(error).toMatch('error');
-    }
-});
-test('short - the data from the promise is peanut butter (async/await)', async () => {
-    await expect(fetchDataAsPromise()).resolves.toBe('peanut butter');
-});
-test('short - the fetch fails with an error (async/await)', async () => {
-    await expect(fetchErrorAsPromise()).rejects.toMatch('error');
-})
-// Testing Asynchronous Code - end
+// Testing Asynchronous Code & Setup and teardown - end
